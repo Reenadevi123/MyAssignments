675ab8f84944844e570363a2e170e8bff465ff89
Reenadevi123 <ramalingamreenadevi@gmail.com>
2024-05-27 09:36:29 -0400

    Add files via upload
    Week1.Day1 assignments

---
+++ Java/Browser.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class Browser {
+//Global Variable
+	int age=30;
+	public static void main(String[] args) {
+		//Loacal variables
+		int noOfTabs=5;
+		boolean pageLoaded=true;
+		char pageName='a';
+		String browserName="Chrome";
+		float browser=123.67F;
+		long number=12321331234l;
+		//Print and concatenation
+		System.out.println("No of Tabs Opened"+noOfTabs);
+		System.out.println(pageLoaded+"\n"+pageName+"\n"+browserName);
+	}
+
+}
---
+++ Java/Firefox.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion=100.2f;
+		String browserName="firefox";
+		boolean isVisible=true;
+		int releaseYear=1998;
+		char browserLogo='f';
+		System.out.println("Browser name:"+browserName+"\n"+"Browser Version:"+browserVersion+"\n"+"Release Year:"+releaseYear+"\n"+"Is Visible:"+isVisible+"\n"+"Logo:"+browserLogo);
+		
+	}
+
+}
